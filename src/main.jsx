544dff27580b16c5b7b1368fa9c02d3bf3571c44
Khunaahai <khunaahai@gmail.com>
2025-08-09 20:14:15 +0800

    add:analitics

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import './index.css';
 
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/next";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
